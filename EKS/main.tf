bc443245de8a11b9dc2052dd45430026968279c0
Sai Teja Ganjikunta <tejags2299@gmail.com>
2023-11-11 13:54:28 +0530

    Moidifying eks main

--- EKS/main.tf
+++ EKS/main.tf
@@ -36,6 +36,8 @@ module "eks" {
   cluster_name    = "pro-eks-cluster"
   cluster_version = "1.27"
 
+  cluster_endpoint_public_access = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
